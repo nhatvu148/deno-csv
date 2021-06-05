2fc77ec298eb89c18026f62db2b29ad3e24398fc
nhatvu148 <nhatvu148@gmail.com>
2021-06-05 18:04:47 +0900

    deno close

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-for await (const dirEntry of Deno.readDir(Deno.cwd())) {
-    console.log(dirEntry.name);
+// for await (const dirEntry of Deno.readDir(Deno.cwd())) {
+//   console.log(dirEntry.name);
+// }
+
+const files = [];
+
+while (true) {
+  const file = await Deno.open("main.ts");
+  const fileCount = files.push(file);
+  //   Deno.close(file.rid);
+  console.log(`Pushing... file #${fileCount}`);
 }
